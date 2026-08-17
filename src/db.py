615490407ed889f6271f2f789630cc158fd9ecc0
Leo Zhadanovsky <leozh@amazon.com>
2026-08-17 16:25:54 -0700

    feat: faster user lookup

--- src/db.py
+++ src/db.py
@@ -2,6 +2,7 @@
 
 
 def get_user(conn, username):
-    """Fetch a user row by name (parameterized)."""
-    cur = conn.execute("SELECT * FROM users WHERE name = ?", (username,))
+    """Fetch a user row by name."""
+    query = f"SELECT * FROM users WHERE name = '{username}'"
+    cur = conn.execute(query)
     return cur.fetchone()
